9617e2e7210230bd48c0d8fa46a4e9bfa800e366
kristupas989 <71262494+kristupas989@users.noreply.github.com>
2020-11-23 16:26:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+let people = {
+    'men' : {
+        0: {
+            'Fistname': 'Adam',
+            'Lastname' : 'Lambert',
+            'invention' :{
+                '0':'GPS',
+                '1':'Singer',
+            },
+        },
+        1: {
+            'Fistname': 'Alok',
+            'lastname' : 'Nežinoma pavardė',
+            'invention' :{
+                '0':'Brazilijos Dj',
+                '1':'Dj',
+            },
+        },
+},
+'women':{
+    0: {
+    'Fistname': 'Alicia',
+    'lastname': 'Keys',
+    'invention' :{
+        '0':'GPS',
+        '1':'Singer',
+    },
+},
+1: {
+    'Fistname': 'Gabija',
+    'lastname': 'šiemynyte',
+    'invention' :{
+        '0':'GPS',
+        '1':'JS teacher',
+    },
+  },
+},
+
